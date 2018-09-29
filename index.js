30e2d068b20bc23534df2221508bd0466b7b335c
iRosSoftware <iros0522@gmail.com>
2018-09-30 00:32:08 +0900

    fix

--- index.js
+++ index.js
@@ -98,9 +98,6 @@ app.post('/clova', clovaMiddleware, clovaSkillHandler);
 app.post('/clova', bodyParser.json(), clovaSkillHandler);
 
 app.post('/webhook', line.middleware(line_config), (req, res, next) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('');
-
     let body = '';
     req.on('data', (chunk) => {
         body += chunk;
